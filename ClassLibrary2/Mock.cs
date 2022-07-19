f6bbaab1e06caa7b738f68a9e82465820de2530b
Julio Cesar Ambriz Quijada <julio-cesar.ambriz@atos.net>
2022-07-19 16:05:44 -0500

    added mock function to factorial method

---
+++ ClassLibrary2/Mock.cs
@@ -0,0 +1,26 @@
+﻿
+
+using Calculators;
+using Moq;
+using Xunit;
+
+namespace UnitTesting
+{
+    public class Mock
+    {
+        [Fact]
+        public void FactorialShouldWorkd()
+        {
+            int number = 5;
+            int expected = 120;
+            Mock<ICalculator> calculator = new Mock<ICalculator>();
+            Series series = new Series(calculator.Object);
+            calculator.Setup(c => c.Mul(number, series.Factorial(number - 1))).Returns(expected);
+
+
+            int actual = series.Factorial(number);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
